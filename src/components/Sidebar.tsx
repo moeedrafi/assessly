03fad6159a90ed4417aa7cd0a0862feafe5f671e
Moeed Rafi <moeedrafi95@gmail.com>
2025-12-29 11:11:28 +0500

    ui: Dashboard Page

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,41 @@
+import {
+  History,
+  LayoutDashboard,
+  TestTube,
+  TextSelectionIcon,
+} from "lucide-react";
+import { SidebarLink } from "./SidebarLink";
+
+export const Sidebar = () => {
+  return (
+    <aside className="hidden md:flex flex-col border-r border-color min-h-screen bg-bg py-6">
+      <div className="p-6">
+        <div className="text-4xl text-center font-bold uppercase leading-[1em] tracking-wide">
+          Assesly
+        </div>
+      </div>
+
+      {/* LINKS */}
+      <nav className="flex-1 px-4">
+        <ul className="space-y-3">
+          <SidebarLink
+            href="/dashboard"
+            icon={<LayoutDashboard />}
+            label="Dashboard"
+          />
+          <SidebarLink href="/quizzes" icon={<TestTube />} label="Quizzes" />
+          <SidebarLink
+            href="/courses"
+            icon={<TextSelectionIcon />}
+            label="Courses"
+          />
+          <SidebarLink href="/history" icon={<History />} label="History" />
+        </ul>
+      </nav>
+
+      <button className="w-1/2 self-center bg-light p-2 border border-color shadow rounded-md hover:-translate-y-0.5 transition-transform">
+        Sign Out
+      </button>
+    </aside>
+  );
+};
---
+++ src/components/SidebarLink.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+
+export const SidebarLink = ({
+  href,
+  icon,
+  label,
+}: {
+  href: string;
+  icon: React.ReactNode;
+  label: string;
+}) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={twMerge(
+          "flex items-center gap-3 rounded-lg p-4 text-sm font-lato font-medium transition-colors",
+          isActive
+            ? "bg-light text-text ring-1 ring-primary"
+            : "text-muted-foreground hover:text-text hover:bg-light",
+          "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary"
+        )}
+      >
+        <span className="h-5 w-5">{icon}</span>
+        <span>{label}</span>
+      </Link>
+    </li>
+  );
+};
